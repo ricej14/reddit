16dbc62457517b0b083cacae23e431790ba36692
John <ricej14@mailbox.winthrop.edu>
2021-03-08 00:02:08 -0500

    05a - render static data from an array object

--- client/main.js
+++ client/main.js
@@ -3,11 +3,36 @@ import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
 Meteor.startup(function(){
-	let name = 'newman';
+
+	const renderSomeParagraphs = function() {
+		return [<p key='1'>p 1</p>, <p key='2'>p 2</p>, <p key='3'>p 3</p>];
+	};
+
+	const renderArrayMap = function() {
+		let numbers = [{val: 1}, {val: 2}, {val: 3}];
+		let newNumbers = numbers.map(function(number) {
+			return number.val - 1;
+		});
+		console.log(newNumbers);
+	};
+
+	const renderPosts = function(passed_posts) {
+		console.log(passed_posts);
+		let numbers = [{val: 11}, {val: 23}, {val: 37}];
+		let newNumbers = numbers.map(function(number) {
+			return <p key={number.val}>{number.val}</p>;
+		});
+		return newNumbers;
+	};
+
+	let title = '441 reddit';
 	let jsx = (
 				<div>
-					<p>this is from the main.js</p>
-					<p>Hello {name}</p>
+					<h1>{title}</h1>
+					{/* [<p key='1'>p 1</p>, <p key='2'>p 2</p>, <p key='3'>p 3</p>] */}
+					{renderSomeParagraphs()}
+					{renderArrayMap()}
+					{renderPosts('hello')}
 				</div>
 				);
 
