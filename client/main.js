e243407a9f5b5f9d45a8d038dedafa691410ca4d
John <ricej14@mailbox.winthrop.edu>
2021-03-08 00:14:18 -0500

    05b - challenge render object (topic/votes)

--- client/main.js
+++ client/main.js
@@ -2,39 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
+const renderPosts = function(passed_posts) {
+	console.log(passed_posts);
+	let formattedPosts = passed_posts.map(function(post) {
+		return <p key={post._id}>{post.topic} have {post.votes} vote[s]</p>;
+	});
+	return formattedPosts;
+};
+
 Meteor.startup(function(){
 
-	const renderSomeParagraphs = function() {
-		return [<p key='1'>p 1</p>, <p key='2'>p 2</p>, <p key='3'>p 3</p>];
-	};
-
-	const renderArrayMap = function() {
-		let numbers = [{val: 1}, {val: 2}, {val: 3}];
-		let newNumbers = numbers.map(function(number) {
-			return number.val - 1;
-		});
-		console.log(newNumbers);
-	};
-
-	const renderPosts = function(passed_posts) {
-		console.log(passed_posts);
-		let numbers = [{val: 11}, {val: 23}, {val: 37}];
-		let newNumbers = numbers.map(function(number) {
-			return <p key={number.val}>{number.val}</p>;
-		});
-		return newNumbers;
-	};
+	const posts = [  {
+							_id: '01',
+							topic: 'cats',
+							votes: 5
+						},
+						{
+							_id: '02',
+							topic: 'dogs',
+							votes: 2
+						},
+						{
+							_id: '03',
+							topic: 'birds',
+							votes: 17
+						}
+	];
 
 	let title = '441 reddit';
 	let jsx = (
 				<div>
 					<h1>{title}</h1>
-					{/* [<p key='1'>p 1</p>, <p key='2'>p 2</p>, <p key='3'>p 3</p>] */}
-					{renderSomeParagraphs()}
-					{renderArrayMap()}
-					{renderPosts('hello')}
+					{/*renderPosts('hello')*/}
+					{renderPosts(posts)}
 				</div>
 				);
-
 	ReactDOM.render(jsx, document.getElementById('content'));
 });
