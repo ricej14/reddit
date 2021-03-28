1bf5441db6e100f9510da8a0e02bc0f567794557
John <ricej14@mailbox.winthrop.edu>
2021-03-28 17:02:35 -0400

    13a - ES6 intro

--- server/main.js
+++ server/main.js
@@ -2,5 +2,37 @@ import {Meteor} from 'meteor/meteor';
 import {UP_Collection_Access} from './../imports/api/user_posts.js';
 
 Meteor.startup(() => {
+	class Person {
 
+	};
+	let me = new Person();
+	console.log(me);
+
+	class Person2 {
+		constructor(name) {
+			this.name = name;
+		}
+	};
+	let me2 = new Person2('Michael');
+	console.log(me2);
+
+	class Person3 {
+		constructor(name = 'anonymous') {
+			this.name = name;
+		}
+	};
+	let me3 = new Person3();
+	let me3b = new Person3('passed name');
+	console.log(me3, me3b);
+
+	class Person4 {
+		constructor(name = 'anonymous') {
+			this.name = name;
+		}
+		getGreeting() {
+			return `hi, I am ${this.name}`;
+		}
+	};
+	let me4 = new Person4('michael');
+	console.log(me4.getGreeting());
 });
