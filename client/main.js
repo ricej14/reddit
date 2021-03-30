336738a4e9a0e0237b724dc4ce1707fc6d8470ff
John <ricej14@mailbox.winthrop.edu>
2021-03-30 00:39:41 -0400

    18a - challenge 1 - create a topic component with dummy text

--- client/main.js
+++ client/main.js
@@ -4,23 +4,25 @@ import {Meteor} from 'meteor/meteor';
 import {UP_Collection_Access} from './../imports/api/user_posts.js';
 import TitleBar from './../imports/ui/TitleBar.js';
 import AddTopics from './../imports/ui/AddTopics.js';
+import RenderPost from './../imports/ui/RenderPost.js';
 
 const renderPosts = (passed_posts) => {
 	//console.log(passed_posts);
 	let formattedPosts = passed_posts.map((post) => {
-		return (
-			<p key={post._id}>{post.topic} have {post.votes} vote[s] {''}
-				<button onClick={() => {
-					UP_Collection_Access.update({_id: post._id}, {$inc: {votes: 1}})
-				}}>+1</button>
-				<button onClick={() => {
-					UP_Collection_Access.update({_id: post._id}, {$inc: {votes: -1}})
-				}}>-1</button>
-				<button onClick={() => {
-					UP_Collection_Access.remove({_id: post._id})
-				}}>X</button>
-			</p>
-		);
+		return <RenderPost key={post._id} />
+		// return (
+		// 	<p key={post._id}>{post.topic} have {post.votes} vote[s] {''}
+		// 		<button onClick={() => {
+		// 			UP_Collection_Access.update({_id: post._id}, {$inc: {votes: 1}})
+		// 		}}>+1</button>
+		// 		<button onClick={() => {
+		// 			UP_Collection_Access.update({_id: post._id}, {$inc: {votes: -1}})
+		// 		}}>-1</button>
+		// 		<button onClick={() => {
+		// 			UP_Collection_Access.remove({_id: post._id})
+		// 		}}>X</button>
+		// 	</p>
+		// );
 	});
 	return formattedPosts;
 };
@@ -31,8 +33,8 @@ Meteor.startup(() => {
 		let title = '441 reddit';
 		let jsx = (
 					<div>
-						<TitleBar title={title} moderator='newman'/>
-						<AddTopics votes={10}/>
+						<TitleBar title={title} moderator='newman' />
+						<AddTopics />
 						{renderPosts(allPostsInDB)}
 					</div>
 					);
--- server/main.js
+++ server/main.js
@@ -2,13 +2,5 @@ import {Meteor} from 'meteor/meteor';
 import {UP_Collection_Access} from './../imports/api/user_posts.js';
 
 Meteor.startup(() => {
-	let basic_object = {
-		moderator: 'newman',
-		print_moderator() {
-			console.log(`Moderator: ${this.moderator}`)
-		}
-	};
-	basic_object.print_moderator();
-	setTimeout(basic_object.print_moderator, 250);
-	setTimeout(basic_object.print_moderator.bind(basic_object), 500);
+	
 });
