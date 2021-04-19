90de7894f68608c1975f3c85eb40a45852532a30
John <ricej14@mailbox.winthrop.edu>
2021-04-18 22:58:04 -0400

    33a - ES6 intro to object spread operator & object property shorthand

--- server/main.js
+++ server/main.js
@@ -2,5 +2,44 @@ import {Meteor} from 'meteor/meteor';
 import {UP_Collection_Access} from './../imports/api/user_posts.js';
 
 Meteor.startup(() => {
-	
+
+});
+
+let person_one = {
+	name: 'Newman',
+	company: 'Post Office',
+	fav_num: 43
+};
+console.log(person_one);
+
+let person_two = {
+	fav_num: 23
+};
+console.log(person_two);
+
+let person_three = {
+	...person_one,
+	fav_num: 17
+};
+console.log(person_three);
+
+let pet = 'cat';
+let type = 'tabby';
+let animal = {
+	pet,
+	type
+};
+console.log(animal);
+
+let car = {
+	color: 'green',
+	doors: 2
+};
+let car_year = 1999;
+
+console.log({
+	...car,
+	color: 'red',
+	car_year,
+	make: 'AMC'
 });
